c7a458f9e502e2cc8100eb529f2ee2bc0079cc59
Raif Kablaoui <atlassian@kp.technology>
2023-12-05 22:37:04 +0100

    setup beanstalk

--- .platform/hooks/predeploy/01_migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
-
-source /etc/profile.d/sh.local
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
